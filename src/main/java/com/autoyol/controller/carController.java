187b8fce0577549861c545cd2992311513c37f25
maqi <qi.ma@atzuche.cn>
2019-04-30 17:37:49 +0800

    Revert "借车"
    This reverts commit cccdfa6

--- src/main/java/com/autoyol/controller/carController.java
+++ src/main/java/com/autoyol/controller/carController.java
@@ -3,16 +3,25 @@ package com.autoyol.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.autoyol.dao.CarMapper;
+import com.autoyol.dao.MemberMapper;
+import com.autoyol.entity.Car;
+import com.autoyol.entity.Member;
 import com.autoyol.entity.PathIP;
 import com.autoyol.entity.Result;
 import com.autoyol.service.CarService;
 import com.autoyol.util.SetDateSourceUtil;
 import com.autoyol.util.ToolUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/car")
@@ -20,7 +29,10 @@ public class carController {
     @Resource
     private CarService carService;
     @Resource
-    private CarMapper carMapper;
+    private CarMapper CarMapper;
+    @Resource
+    private MemberMapper memberMapper;
+    private static final Logger logger = LoggerFactory.getLogger(carController.class);
 
 
     /**
@@ -46,4 +58,54 @@ public class carController {
         return result;
     }
 
+
+    /**
+     * 更换车牌
+     * @param carPara
+     * @param mobile
+     * @param environment
+     */
+    @RequestMapping(value = {"/updatecarmemno"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public Result UpdateCarMemno(String environment, String mobile, String carPara){
+        SetDateSourceUtil.setDataSourceName(environment);
+        Result result = new Result();
+        if (!ToolUtil.isMobile(mobile)){
+            result = ToolUtil.checkMobile(mobile);
+            return result;
+        }
+
+        Car car = CarMapper.selectCarInfo(carPara);
+        if(car == null){
+            logger.info("车辆不存在");
+            result.setStatus(1);
+            result.setMsg("success");
+            result.setData("车辆（" + carPara + "）不存在，请确认后再试");
+            return result;
+        }
+
+        List<Member> memberList = memberMapper.selectMemberInfoByMobile(mobile);
+        if (memberList.size() <= 0){
+            result.setStatus(1);
+            result.setMsg("success");
+            result.setData("会员（" + mobile + "）不存在，请确认后再试");
+            return result;
+        }
+
+        Map<String,String> paramap = new HashMap<>();
+        paramap.put("carPara",carPara);
+        paramap.put("mobile",mobile);
+        try {
+            CarMapper.UpdateCarIsMemno(paramap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        logger.info("更换车牌_Result",JSON.toJSONString(result.getData()));
+        result.setStatus(1);
+        result.setMsg("success");
+        result.setData("恭喜" + mobile + "用户，偷车成功!!!");
+        return result;
+    }
 }
+
+
