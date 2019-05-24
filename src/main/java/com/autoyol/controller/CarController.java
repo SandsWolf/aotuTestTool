5629bfe6b8d62034b3d9cd78938c9ac96e52f466
wenli.shi <123456789>
2019-05-24 10:02:20 +0800

    师~

--- src/main/java/com/autoyol/controller/CarController.java
+++ src/main/java/com/autoyol/controller/CarController.java
@@ -1,12 +1,16 @@
 package com.autoyol.controller;
 
 
+import ch.qos.logback.core.joran.conditional.ElseAction;
 import com.alibaba.fastjson.JSON;
 import com.autoyol.dao.CarMapper;
 import com.autoyol.dao.MemberMapper;
 import com.autoyol.entity.Car;
 import com.autoyol.entity.Member;
+import com.autoyol.entity.PathIP;
 import com.autoyol.entity.Result;
+import com.autoyol.http.HttpResponse;
+import com.autoyol.http.HttpUtils;
 import com.autoyol.service.CarService;
 import com.autoyol.util.SetDateSourceUtil;
 import com.autoyol.util.ToolUtil;
@@ -37,6 +41,7 @@ public class CarController {
 
     /**
      * 重置车辆信息，使车辆可租
+     *
      * @param environment
      * @param car_no
      * @return
@@ -44,9 +49,9 @@ public class CarController {
 
     @RequestMapping("/reSetRent")
     @ResponseBody
-    public Result reSetRent(String environment, String car_no){
+    public Result reSetRent(String environment, String car_no) {
         Result result = new Result();
-        if("线上".equals(environment)){
+        if ("线上".equals(environment)) {
             result.setStatus(1);
             result.setMsg("success");
             result.setData("线上环境只能做查询操作");
@@ -62,22 +67,23 @@ public class CarController {
 
     /**
      * 更换车牌
+     *
      * @param carPara
      * @param mobile
      * @param environment
      */
     @RequestMapping(value = {"/updatecarmemno"}, method = {RequestMethod.POST})
     @ResponseBody
-    public Result UpdateCarMemno(String environment, String mobile, String carPara){
+    public Result UpdateCarMemno(String environment, String mobile, String carPara) {
         SetDateSourceUtil.setDataSourceName(environment);
         Result result = new Result();
-        if (!ToolUtil.isMobile(mobile)){
+        if (!ToolUtil.isMobile(mobile)) {
             result = ToolUtil.checkMobile(mobile);
             return result;
         }
 
         Car car = CarMapper.selectCarInfo(carPara);
-        if(car == null){
+        if (car == null) {
             logger.info("车辆不存在");
             result.setStatus(1);
             result.setMsg("success");
@@ -86,22 +92,22 @@ public class CarController {
         }
 
         List<Member> memberList = memberMapper.selectMemberInfoByMobile(mobile);
-        if (memberList.size() <= 0){
+        if (memberList.size() <= 0) {
             result.setStatus(1);
             result.setMsg("success");
             result.setData("会员（" + mobile + "）不存在，请确认后再试");
             return result;
         }
 
-        Map<String,String> paramap = new HashMap<>();
-        paramap.put("carPara",carPara);
-        paramap.put("mobile",mobile);
+        Map<String, String> paramap = new HashMap<>();
+        paramap.put("carPara", carPara);
+        paramap.put("mobile", mobile);
         try {
             CarMapper.UpdateCarIsMemno(paramap);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logger.info("更换车牌_Result",JSON.toJSONString(result.getData()));
+        logger.info("更换车牌_Result", JSON.toJSONString(result.getData()));
         result.setStatus(000000);
         result.setMsg("success");
         result.setData("恭喜" + mobile + "用户，偷车成功!!!");
@@ -110,21 +116,22 @@ public class CarController {
 
     /**
      * 统计该手机号下车的数量
+     *
      * @param mobile
      * @return 车的数量
      */
     @RequestMapping(value = {"/memcarcount"}, method = {RequestMethod.POST})
     @ResponseBody
-    public Result MemberCarCount(String environment,String mobile){
+    public Result MemberCarCount(String environment, String mobile) {
         SetDateSourceUtil.setDataSourceName(environment);
         Result result = new Result();
-        if (!ToolUtil.isMobile(mobile)){
+        if (!ToolUtil.isMobile(mobile)) {
             result = ToolUtil.checkMobile(mobile);
             return result;
         }
         List<Member> memberList = memberMapper.selectMemberInfoByMobile(mobile);
 
-        if (memberList.size() <= 0){
+        if (memberList.size() <= 0) {
             result.setStatus(1);
             result.setMsg("success");
             result.setData("会员（" + mobile + "）不存在，请确认后再试");
@@ -140,10 +147,70 @@ public class CarController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logger.info("统计会员名下车的总数_Result",JSON.toJSONString(result.getData()));
+        logger.info("统计会员名下车的总数_Result", JSON.toJSONString(result.getData()));
         result.setStatus(000000);
         result.setMsg("success");
-        result.setData(mobile + "用户名下车的总数量为：" + count + "car_no：" +  carregnolist.toString());
+        result.setData(mobile + "用户名下车的总数量为：" + count + "car_no：" + carregnolist.toString());
+        return result;
+    }
+
+
+    /**
+     * 收藏车辆
+     *
+     * @param car
+     * @return 收藏车辆
+     */
+    @RequestMapping(value = {"/favorites"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public Result Favourites(String environment, String carNo, String mobile) {
+        SetDateSourceUtil.setDataSourceName(environment);
+        PathIP pathIP = ToolUtil.getIP(environment);
+        List<Member> memberList = memberMapper.selectMemberInfoByMobile(mobile);
+
+        Result result = new Result();
+
+        if (memberList.size() <= 0) {
+            result.setStatus(1);
+            result.setMsg("success");
+            result.setData("会员（" + mobile + "）不存在，请确认后再试");
+            return result;
+        }
+
+        String memNo = memberList.get(0).getReg_no();
+        String token = memberList.get(0).getToken();
+
+
+        String url = pathIP.getServerIP() + "/car/favorites";
+
+        Map<String, String> headerMap = new HashMap<String, String>();
+        headerMap.put("Content-Type", "application/json; charset=utf-8");
+        headerMap.put("connection", "Keep-Alive");
+        headerMap.put("User-Agent", "AutoyolEs_console");
+        headerMap.put("Accept", "application/json;version=3.0;compress=false");
+
+        Map<String, Object> paraMap = new HashMap<String, Object>();
+        paraMap.put("publicCityCode", "021");
+        paraMap.put("IMEI", "EF818E1E-771C-469B-A769-869C6153EFC7");
+        paraMap.put("AppVersion", "91");
+        paraMap.put("PublicLatitude", "31.171986");
+        paraMap.put("PublicLongitude", "121.409134");
+        paraMap.put("deviceName", "iPhone7Plus");
+        paraMap.put("token", token);
+        paraMap.put("OsVersion", "12.2");
+        paraMap.put("mem_no", memNo);
+        paraMap.put("OS", "IOS");
+        paraMap.put("ModuleName", "car");
+        paraMap.put("publicToken", token);
+        paraMap.put("requestId", "EF818E1E-771C-469B-A769-869C6153EFC74789206853931208794");
+        paraMap.put("carNo", carNo);
+        HttpResponse httpResult = HttpUtils.post(headerMap, url, paraMap, false, false);
+
+        result.setStatus(0);
+        result.setMsg("success");
+        result.setData("收藏成功" + httpResult.getResponseBodyObject().toString());
+
+
         return result;
     }
 }
--- src/main/webapp/scripts/car.js
+++ src/main/webapp/scripts/car.js
@@ -163,3 +163,69 @@ function selectMemCarCount(event){
         }
     });
 }
+
+
+
+
+//收藏车辆
+function selectMemCarCount(event){
+    $("#result_msg").empty();
+    var environment = $("#set_environment").val();
+    var mobile = $("#input_mobile").val().trim();
+    var carNo = $("#input_value").val().trim();
+
+
+    if(environment == "-请选择-"){
+        $(".opacity_bg").show();//弹出对话框后背景置灰，防止误操作
+        $("#can").load("alert/alert_environment.html");//显示对话框
+        return;
+    }
+
+    if(environment == "线上"){
+        $(".opacity_bg").show();//弹出对话框后背景置灰，防止误操作
+        $("#can").load("alert/alert_online.html");//显示对话框
+        return;
+    }
+
+    if(mobile == ""){
+        $(".opacity_bg").show();//弹出对话框后背景置灰，防止误操作
+        $("#can").load("alert/alert_phone.html");//显示对话框
+        return;
+    }
+
+    if(carNo == ""){
+        $(".opacity_bg").show();//弹出对话框后背景置灰，防止误操作
+        $("#can").load("alert/alert_phone.html");//显示对话框
+        return;
+    }
+
+    $.ajax({
+        url:event.data.ip + "/car/favorites",
+        type:"post",
+        data:{"environment":environment,"mobile":mobile,"carNo":carNo},
+        dataType:"json",
+        success:function(result){
+            if(result.status==1){
+                $("#result_msg").empty();
+
+                var resultMsg = result.data;
+                var li = "<br><span class='sign_span'><b>结果：<br></span><br><span class='sign_span' style='color:red;'>" + resultMsg + "</span></b></span>";
+                var $li = $(li);
+                $("#result_msg").append($li);
+            }
+
+            if(result.status==0){
+                $("#result_msg").empty();
+
+                var resultMsg = result.data;
+                var li = "<br><span class='sign_span'><b>结果：<br>" + resultMsg + "</b></span>";
+                var $li = $(li);
+                $("#result_msg").append($li);
+            }
+        },
+        error:function(){
+            $(".opacity_bg").show();//弹出对话框后背景置灰，防止误操作
+            $("#can").load("alert/alert_error.html");//显示对话框
+        }
+    });
+}
